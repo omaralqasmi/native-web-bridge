8dd3b3fbe48e770bedf51d6d9da2ea3b6706e195
Omar Al Qasmi <omar.alqasmi@outlook.com>
2026-05-12 13:49:56 +0400

    1.0.6

--- native-web-bridge-android/build.gradle.kts
+++ native-web-bridge-android/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+    id("com.android.library") version "8.1.1"
+    id("org.jetbrains.kotlin.android") version "1.9.0"
+    id("maven-publish")
 }
 
 android {
